6e25cc2227278b01aa1173df4a6d310f7b4f27c2
KovalevDima <onarainydayillwinanyway@gmail.com>
2026-07-26 02:04:04 +0000

    Add original go package build

--- flake.nix
+++ flake.nix
@@ -1,7 +1,13 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    ixy-languages-go = {
+      url = "github:ixy-languages/ixy.go/master";
+      flake = false;
+    };
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, ... } @ inputs:
   let
     forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
   in
@@ -10,7 +16,42 @@
       let
         pkgs = import nixpkgs { inherit system; };
       in {
-        default = pkgs.haskellPackages.callCabal2nix "ixy-haskell" ./ixy-haskell { };
+        ixy-haskell = pkgs.haskellPackages.callCabal2nix "ixy-haskell" ./ixy-haskell { };
+
+        ixy-go = pkgs.stdenv.mkDerivation {
+          name = "forwarder";
+          src = inputs.ixy-languages-go;
+          version = "0.1";
+
+          nativeBuildInputs = [ pkgs.go ];
+
+          patchPhase = ''
+              cat > ./go.mod <<'EOF'
+              module github.com/ixy-languages/ixy.go
+          
+              go 1.26
+
+
+              require (
+              )
+              EOF
+            '';
+
+          
+          buildPhase = ''
+            export GOCACHE=/tmp/go-build
+            mkdir -p "$GOCACHE"
+
+            cd ./fwd/
+            go build -o forwarder .
+            cd ../
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp fwd/forwarder $out/bin/
+          '';
+        };
       });
   };
 }
